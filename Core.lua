be48c5e35312d5ebe5cc107e33d613b8a4f4e22b
Adirelle <adirelle@gmail.com>
2009-09-01 17:09:55 +0200

    Added aura icons. Fixed frame aspect for out-of-range unit with visible border. LibSharedMedia-3.0 is now optionak.

--- Core.lua
+++ Core.lua
@@ -4,13 +4,25 @@ Adirelle's oUF raid layout
 All rights reserved.
 --]=]
 
+local WIDTH = 80
+local BORDER_WIDTH = 1
+local ICON_SIZE = 16
+local HEIGHT = 20
+local SPACING = 2
+
 local oUF = assert(_G.oUF, "oUF_Adirelle requires oUF")
-local lsm = LibStub('LibSharedMedia-3.0')
+local lsm = LibStub('LibSharedMedia-3.0', true)
+
+local statusbarTexture = lsm and lsm:Fetch("statusbar", false) or [[Interface\TargetingFrame\UI-StatusBar]]
 
 local backdrop = {
 	bgFile = "Interface\\Tooltips\\UI-Tooltip-Background", tile = true, tileSize = 16,
-	--edgeFile = "Interface\\Tooltips\\UI-Tooltip-Border", edgeSize = 8,
 	insets = {left = 0, right = 0, top = 0, bottom = 0},
+}
+
+local borderBackdrop = {
+	edgeFile = [[Interface\Addons\oUF_Adirelle\white16x16]], 
+	edgeSize = BORDER_WIDTH,	insets = {left = BORDER_WIDTH, right = BORDER_WIDTH, top = BORDER_WIDTH, bottom = BORDER_WIDTH},
 }
 
 local UnitClass, UnitIsConnected = UnitClass, UnitIsConnected
@@ -46,7 +58,58 @@ local function UpdateHealth(self, event, unit, bar, current, max)
 	end
 end
 
-do
+local function UpdateTextures(self)
+	self.Health:SetStatusBarTexture(statusbarTexture)
+	self.Health:SetStatusBarColor(0, 0, 0, 0.75)
+	self.Health.bg:SetTexture(statusbarTexture)
+end
+
+local function SpawnIcon(self)
+	local	icon = CreateFrame("Frame", nil, self)
+	icon:SetWidth(ICON_SIZE)
+	icon:SetHeight(ICON_SIZE)
+	
+	local texture = icon:CreateTexture(nil, "OVERLAY")
+	texture:SetAllPoints(icon)
+	texture:SetTexCoord(0.05, 0.95, 0.05, 0.95)
+	texture:SetTexture(1,1,1,0)	
+	icon.Texture = texture	
+
+	local cooldown = CreateFrame("Cooldown", nil, icon, "CooldownFrameTemplate")
+	cooldown:SetAllPoints(texture)
+	cooldown:SetReverse(true)
+	icon.Cooldown = cooldown
+
+	local stack = icon:CreateFontString(nil, "OVERLAY", "GameFontNormal")
+	stack:SetAllPoints(texture)
+	stack:SetJustifyH("CENTER")
+	stack:SetJustifyV("MIDDLE")
+	stack:SetFont(GameFontNormal:GetFont(), 10, "OUTLINE")
+	stack:SetTextColor(1, 1, 1, 1)
+	icon.Stack = stack
+	
+	local border = CreateFrame("Frame", nil, icon)
+	border:SetFrameStrata("BACKGROUND")
+	border:SetPoint("TOPLEFT", icon, "TOPLEFT", -BORDER_WIDTH, BORDER_WIDTH)
+	border:SetPoint("BOTTOMRIGHT", icon, "BOTTOMRIGHT", BORDER_WIDTH, -BORDER_WIDTH)
+	border:SetBackdrop(borderBackdrop)
+	border:SetBackdropColor(0, 0, 0, 0)
+	border:SetBackdropBorderColor(1, 1, 1, 1)
+	border.SetColor = border.SetBackdropBorderColor
+	icon.Border = border
+	
+	return icon
+end
+
+local function TestMyAura(spellId)
+	local spellName = GetSpellInfo(spellId)
+	assert(spellName, "invalid spell id: "..spellId)
+	return function(unit)
+		local name, _, texture, count, _, duration, expirationTime, caster = UnitAura(unit, spellName)
+		if name and UnitIsUnit(caster, "player") then
+			return texture, count, expirationTime-duration, duration, 1, 0, 0
+		end
+	end
 end
 
 local function InitFrame(settings, self, unit)
@@ -58,54 +121,83 @@ local function InitFrame(settings, self, unit)
 	
 	self:SetBackdrop(backdrop)
 	self:SetBackdropColor(0, 0, 0, 1)
-	self:SetBackdropBorderColor(1, 0, 0, 1)
-	
-	local texture = lsm:Fetch("statusbar", false)
+	self:SetBackdropBorderColor(0, 0, 0, 1)
 	
 	-- Health bar
 	local hp = CreateFrame("StatusBar", nil, self)
 	hp:SetAllPoints(self)
-	hp:SetStatusBarTexture(texture)
-	hp:SetStatusBarColor(0, 0, 0, 0.75)
 	hp.frequentUpdates = true
 		
 	local hpbg = hp:CreateTexture(nil, "BACKGROUND")
 	hpbg:SetAllPoints(hp)
-	hpbg:SetTexture(texture)
 	hpbg:SetAlpha(1)
 	hp.bg = hpbg
 
-	lsm.RegisterCallback(self, 'LibSharedMedia_SetGlobal', function(_, media, value)
-		if media == "statusbar" then
-			local texture = lsm:Fetch("statusbar", value)
-			hp:SetStatusBarTexture(texture)
-			hp:SetStatusBarColor(0, 0, 0, 0.75)
-			hpbg:SetTexture(texture)
-		end
-	end)
-	
 	self.Health = hp
 	self.OverrideUpdateHealth = UpdateHealth
+
+	UpdateTextures(self)
+	
+	if lsm then
+		lsm.RegisterCallback(self, 'LibSharedMedia_SetGlobal', function(_, media, value)
+			if media == "statusbar" then
+				statusbarTexture = lsm:Fetch("statusbar", value)
+				UpdateTextures(self)
+			end
+		end)
+	end
 	
 	-- Name
-	local name = hp:CreateFontString(nil, "OVERLAY", "GameFontNormal")
+	local name = hp:CreateFontString(nil, "ARTWORK", "GameFontNormal")
 	name:SetAllPoints(self)
-	name:SetJustifyH"CENTER"
+	name:SetJustifyH("CENTER")
+	name:SetJustifyV("MIDDLE")
 	name:SetFont(GameFontNormal:GetFont(), 11)
 	name:SetTextColor(1, 1, 1, 1)
 	self.Name = name
 	
 	-- Border
-	local border = self:CreateTexture(nil, "ARTWORK")
-	border:SetPoint("LEFT", self, "LEFT", -1, 0)
-	border:SetPoint("RIGHT", self, "RIGHT", 1, 0)
-	border:SetPoint("TOP", self, "TOP", 0, 1)
-	border:SetPoint("BOTTOM", self, "BOTTOM", 0, - 1)
-	border:SetTexture(1, 1, 1, 1)
-	border:SetAlpha(1)
-	border:SetVertexColor(0, 0, 0)
+	local border = CreateFrame("Frame", nil, self)
+	border:SetFrameStrata("BACKGROUND")
+	border:SetPoint("TOPLEFT", self, "TOPLEFT", -BORDER_WIDTH, BORDER_WIDTH)
+	border:SetPoint("BOTTOMRIGHT", self, "BOTTOMRIGHT", BORDER_WIDTH, -BORDER_WIDTH)
+	border:SetBackdrop(borderBackdrop)
+	border:SetBackdropColor(0, 0, 0, 0)
+	border:SetBackdropBorderColor(1, 1, 1, 1)
+	border.SetColor = border.SetBackdropBorderColor
 	border:Hide()	 
 	self.Border = border
+	
+	--[=[
+	-- ReadyCheck icon
+	local rc = self:CreateTexture(nil, 'OVERLAY')
+	rc:SetPoint('CENTER', self)
+	rc:SetWidth(HEIGHT)
+	rc:SetHeight(HEIGHT)
+	rc:SetAlpha(1.0)
+	rc:Hide()
+	self.ReadyCheck = rc 
+	--]=]
+	
+	local _, class = UnitClass("player")
+	if class == "HUNTER" then
+		local misdirection = SpawnIcon(self)
+		misdirection:SetPoint("CENTER", self, "CENTER", 0, 0)
+		self:AuraIcon(misdirection, TestMyAura(34477))
+		
+	elseif class == "DRUID" then
+		local rejuv = SpawnIcon(self)
+		rejuv:SetPoint("RIGHT", self, "CENTER", - SPACING * 1.5 - ICON_SIZE, 0)
+		self:AuraIcon(rejuv, TestMyAura(774))
+
+		local regrowth = SpawnIcon(self)
+		regrowth:SetPoint("RIGHT", self, "CENTER", - SPACING / 2, 0)
+		self:AuraIcon(regrowth, TestMyAura(8936))
+
+		local lifebloom = SpawnIcon(self)
+		lifebloom:SetPoint("LEFT", self, "CENTER", SPACING / 2, 0)
+		self:AuraIcon(lifebloom, TestMyAura(33763))
+	end
 	
 	-- Range fading
 	self.Range = true
@@ -115,8 +207,8 @@ end
 
 local style = setmetatable(
 	{
-		["initial-width"] = 80,
-		["initial-height"] = 20,
+		["initial-width"] = WIDTH,
+		["initial-height"] = HEIGHT,
 	}, {
 		__call = InitFrame,
 	}
@@ -125,7 +217,6 @@ local style = setmetatable(
 oUF:RegisterStyle("Adirelle", style)
 oUF:SetActiveStyle("Adirelle")
 
-local spacing = 2
 local raid = {}
 for group = 1, 8 do
 	local header = oUF:Spawn("header", "oUF_Raid" .. group)
@@ -133,10 +224,10 @@ for group = 1, 8 do
 		"showRaid", true,
 		"groupFilter", group,
 		"point", "LEFT",
-		"xOffset", spacing
+		"xOffset", SPACING
 	)
 	if group > 1 then
-		header:SetPoint("BOTTOMLEFT", raid[group - 1], "TOPLEFT", 0, spacing)
+		header:SetPoint("BOTTOMLEFT", raid[group - 1], "TOPLEFT", 0, SPACING)
 	end
 	header:Show()
 	raid[group] = header
@@ -150,9 +241,9 @@ do
 		"showSolo", true,
 		"groupFilter", 1,
 		"point", "LEFT",
-		"xOffset", spacing
+		"xOffset", SPACING
 	)
-	header:SetPoint("BOTTOMLEFT", raid[1], "TOPLEFT", 0, spacing)
+	header:SetPoint("BOTTOMLEFT", raid[1], "TOPLEFT", 0, SPACING)
 	header:Show()
 	raid['PartyPets'] = header
 end
--- Icon.lua
+++ Icon.lua
@@ -1,1 +1,75 @@
--- To do...
+--[=[
+--]=]
+
+local oUF = assert(_G.oUF, "oUF_Adirelle requires oUF")
+
+local function UpdateIcon(self, unit, icon, func)
+	local texture, count, start, duration, r, g, b = func(unit)
+	if not texture then
+		return icon:Hide()
+	end
+	icon.Texture:SetTexture(texture)
+	local cooldown, stack, border = icon.Cooldown, icon.Stack, icon.Border
+	if cooldown then
+		if start and duration then
+			cooldown:SetCooldown(start, duration)
+			cooldown:Show()
+		else
+			cooldown:Hiden()
+		end
+	end
+	if stack then
+		if (tonumber(count) or 0) > 1 then
+			stack:SetText(count)
+			stack:Show()
+		else
+			stack:Hide()
+		end
+	end
+	if border then
+		if r and g and b then
+			border:SetColor(r, g, b, 1)
+			border:Show()
+		else
+			border:Hide()
+		end
+	end
+	icon:Show()
+end
+
+local function Update(self, event, unit)
+	if unit and unit ~= self.unit then return end
+	unit = unit or self.unit
+	for icon, func in pairs(self.AuraIcons) do
+		UpdateIcon(self, unit, icon, func)
+	end
+end
+
+local function Enable(self)
+	local icons = self.AuraIcons and next(self.AuraIcons)
+	if icons then
+		self:RegisterEvent('UNIT_AURA', Update)
+	end
+end
+
+local function Disable(self)
+	local icons = self.AuraIcons
+	if icons then
+		self:UnregisterEvent('UNIT_AURA')
+		for icon in pairs(icons) do
+			icon:Hide()
+		end
+	end
+end
+
+local function AuraIcon(self, icon, func, ...)
+	assert(type(icon) == "table", "icon should be a table, not "..type(icon))
+	assert(type(func) == "function", "func should be a function ,not "..type(func))
+	self.AuraIcons = self.AuraIcons or {}
+	self.AuraIcons[icon] = func
+	return icon
+end
+
+oUF.AuraIcon = AuraIcon
+oUF:AddElement('Adirelle_AuraIcons', Update, Enable, Disable)
+
