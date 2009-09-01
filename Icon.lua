a15fc3d116e8d980eff67d6e81c26f13f1bc6d56
Adirelle <adirelle@tagada-team.net>
2009-09-02 00:25:34 +0200

    Added LibHealComm-3.0 support. Fixed border widths. Added cureable debuffs for druids.

--- Icon.lua
+++ Icon.lua
@@ -3,12 +3,40 @@
 
 local oUF = assert(_G.oUF, "oUF_Adirelle requires oUF")
 
+local function UpdateAlpha(icon)
+	local now = GetTime()
+	local alpha = 1
+	if icon.expireTime - now < icon.threshold then
+		alpha = 2 * (now % 1)
+		if alpha > 1 then
+			alpha = 2 - alpha
+		end
+	end
+	if alpha ~= icon:GetAlpha() then
+		icon:SetAlpha(alpha)	
+	end
+end
+
+local function EndBlinking(icon)
+	icon:SetAlpha(1)
+	icon:SetScript('OnUpdate', nil)
+	icon:SetScript('OnHide', nil)	
+end
+
 local function UpdateIcon(self, unit, icon, func)
 	local texture, count, start, duration, r, g, b = func(unit)
 	if not texture then
 		return icon:Hide()
 	end
 	icon.Texture:SetTexture(texture)
+	if self.iconBlinkThreshold and start and duration then
+		icon.expireTime = start + duration
+		icon.threshold = self.iconBlinkThreshold
+		icon:SetScript('OnUpdate', UpdateAlpha)
+		icon:SetScript('OnHide', EndBlinking)	
+	else
+		EndBlinking(icon)
+	end
 	local cooldown, stack, border = icon.Cooldown, icon.Stack, icon.Border
 	if cooldown then
 		if start and duration then
