9de8e03085551e32b0e1aa1526857ff349e2c3fe
Adirelle <adirelle@tagada-team.net>
2009-09-03 14:51:30 +0200

    Fixed name not updating properly. Added LibHealComm-4.0 optional support.

--- elements/IncomingHeal.lua
+++ elements/IncomingHeal.lua
@@ -14,16 +14,67 @@ local lhc4, lhc4_minor = LibStub('LibHealComm-4.0', true)
 -- ------------------------------------------------------------------------------
 -- LibHealComm-4.0 support
 -- ------------------------------------------------------------------------------
-if false and lhc4 then
+if lhc4 then
 	major, minor = 'LibHealComm-4.0', lhc4_minor
-	-- TODO
+	local objects = {}
+	local playerHeals = {}
+	local band = bit.band
+	local HEAL_FLAGS = lhc4.ALL_HEALS
+
+	local function UpdateHeals(event, _, healType, _, ...)
+		if band(healType, HEAL_FLAGS) == 0 then return end
+		local unitMap = lhc4:GetGuidUnitMapTable()
+		local units = oUF.units
+		for i = 1, select('#', ...) do
+			local guid = select(i, ...)
+			local unit = guid and unitMap[guid]
+			local frame = unit and units[unit]
+			if frame then
+				frame:UpdateElement('IncomingHeal')
+			end
+		end
+	end
+	
+	local function ModifierChanged(guid)
+		local unitMap = lhc4:GetGuidUnitMapTable()
+		local frame = oUF.units[unitMap[guid] or false]
+		if frame and frame.unit == unit then
+			frame:UpdateElement('IncomingHeal')
+		end
+	end
+
+	function GetIncomingHeal(unit, time)
+		local guid = UnitGUID(unit)
+		local inc = lhc4:GetHealAmount(guid, HEAL_FLAGS, time)
+		if inc then
+			return inc * lhc4:GetHealModifier(guid)
+		else
+			return 0
+		end
+	end
+
+	function DoEnable(self)
+		if not next(objects) then
+			lhc4.RegisterCallback('oUF_IncomingHeal', 'HealComm_HealStarted', UpdateHeals)
+			lhc4.RegisterCallback('oUF_IncomingHeal', 'HealComm_HealUpdated', UpdateHeals)
+			lhc4.RegisterCallback('oUF_IncomingHeal', 'HealComm_HealDelayed', UpdateHeals)
+			lhc4.RegisterCallback('oUF_IncomingHeal', 'HealComm_HealStopped', UpdateHeals)
+			lhc4.RegisterCallback('oUF_IncomingHeal', 'HealComm_ModifierChanged', ModifierChanged)
+		end
+		objects[self] = true
+	end
+
+	function DoDisable(self) 
+		if not next(objects) then
+			lhc4.UnregisterAllCallbacks('oUF_IncomingHeal')
+		end
+	end
 
 -- ------------------------------------------------------------------------------
 -- LibHealComm-3.0 support
 -- ------------------------------------------------------------------------------
 elseif lhc3 then
 	major, minor = 'LibHealComm-3.0', lhc3_minor
-	local lhc = lhc3
 	local playerName = UnitName('player')
 	local objects = {}
 	local playerHeals = {}
@@ -59,15 +110,15 @@ elseif lhc3 then
 	end
 
 	function GetIncomingHeal(unit, time)
-		return lhc:UnitHealModifierGet(unit) * ( (lhc:UnitIncomingHealGet(unit, time) or 0) + (playerHeals[UnitName(unit) or false] or 0))
+		return lhc3:UnitHealModifierGet(unit) * ( (lhc3:UnitIncomingHealGet(unit, time) or 0) + (playerHeals[UnitName(unit) or false] or 0))
 	end
 
 	function DoEnable(self)
 		if not next(objects) then
-			lhc.RegisterCallback('oUF_IncomingHeal', 'HealComm_DirectHealStart', DirectHealStart)
-			lhc.RegisterCallback('oUF_IncomingHeal', 'HealComm_DirectHealDelayed', DirectHealDelayed)
-			lhc.RegisterCallback('oUF_IncomingHeal', 'HealComm_DirectHealStop', DirectHealStop)
-			lhc.RegisterCallback('oUF_IncomingHeal', 'HealComm_HealModifierUpdate', HealModifierUpdate)
+			lhc3.RegisterCallback('oUF_IncomingHeal', 'HealComm_DirectHealStart', DirectHealStart)
+			lhc3.RegisterCallback('oUF_IncomingHeal', 'HealComm_DirectHealDelayed', DirectHealDelayed)
+			lhc3.RegisterCallback('oUF_IncomingHeal', 'HealComm_DirectHealStop', DirectHealStop)
+			lhc3.RegisterCallback('oUF_IncomingHeal', 'HealComm_HealModifierUpdate', HealModifierUpdate)
 		end
 		objects[self] = true
 	end
@@ -76,7 +127,7 @@ elseif lhc3 then
 		if objects[self] then
 			objects[self] = nil
 			if not next(objects) then
-				lhc.UnregisterAllCallbacks('oUF_IncomingHeal')
+				lhc3.UnregisterAllCallbacks('oUF_IncomingHeal')
 			end
 		end
 	end
@@ -96,7 +147,7 @@ local function Enable(self)
 end
 
 local function Disable(self)
-	if self.IncomingHeal and type(self.UpdateIncomingHeal) == "function" then
+	if self.IncomingHeal and type(self.UpdateIncomingHeal) == "function" then	
 		return DoDisable(self)
 	end
 end
@@ -106,7 +157,7 @@ local function Update(self, event, unit)
 	if not heal then return end
 	local current, max, incomingHeal = UnitHealth(unit), UnitHealthMax(unit), 0
 	if UnitIsConnected(unit) and not UnitIsDeadOrGhost(unit) then
-		incomingHeal = GetIncomingHeal(unit, GetTime()+4)
+		incomingHeal = GetIncomingHeal(unit, GetTime()+3)
 	end
 	self:UpdateIncomingHeal(event, unit, heal, current, max, incomingHeal)
 end
