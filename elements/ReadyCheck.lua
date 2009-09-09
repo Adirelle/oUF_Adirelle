57073c8d83c45315365d11d3abe1fc1e09912685
Adirelle <adirelle@gmail.com>
2009-09-09 13:36:08 +0200

    Another attempt to fix ReadyChecks.

--- elements/ReadyCheck.lua
+++ elements/ReadyCheck.lua
@@ -3,79 +3,106 @@ Adirelle's oUF raid layout
 (c) 2009 Adirelle (adirelle@tagada-team.net)
 All rights reserved.
 
-Elements handled: .ReadyCheck	
+Elements handled: .ReadyCheck
 --]=]
 
 local oUF = assert(_G.oUF, "oUF_Adirelle requires oUF")
 
 local textures = {
-	waiting   = READY_CHECK_WAITING_TEXTURE,
-	ready     = READY_CHECK_READY_TEXTURE,
-	not_ready = READY_CHECK_NOT_READY_TEXTURE,
-	afk       = READY_CHECK_AFK_TEXTURE
+	waiting  = READY_CHECK_WAITING_TEXTURE,
+	ready    = READY_CHECK_READY_TEXTURE,
+	notready = READY_CHECK_NOT_READY_TEXTURE,
+	afk      = READY_CHECK_AFK_TEXTURE
 }
 
 local icons = {}
-local timerFrame
-local mmin = math.min
+local fadeOutFrame
 
-local function UpdateAlpha(self)
+local function FadeOutIcons()
 	local now = GetTime()
 	for icon, expireTime in pairs(icons) do
-		local alpha = mmin(expireTime - now, 1)
+		local alpha = math.min(expireTime - now, 1)
 		if alpha <= 0 then
-			alpha = 1
 			icon:Hide()
-			icons[icon] =  nil
-		end
-		if icon:GetAlpha() ~= alpha then
+			icons[icon] = nil
+		else
 			icon:SetAlpha(alpha)
 		end
 	end
 	if not next(icons) then
-		self:Hide()
+		fadeOutFrame:Hide()
 	end
 end
 
-local function Update(self)
-	local status = GetReadyCheckStatus(self.unit)
+local function SetStatus(self, newStatus)
 	local rc = self.ReadyCheck
-	if rc.status == status then return end
-	print('readycheck change', self.unit, 'old:', rc.status, 'new:', status)
-	rc.status = status
-	if status then
-		icons[rc] = nil
-		rc:SetTexture(textures[status])
-		rc:SetAlpha(1.0)
+	local oldStatus = rc.status
+	if oldStatus == newStatus then return end
+	print('ReadyCheck:SetStatus', self.unit, ':', oldStatus, '=>', newStatus)
+	rc.status = newStatus
+	if newStatus == 'waiting' or newStatus == 'ready' or newStatus == 'notready' then
+		print('=> showing texture', textures[newStatus])
+		rc:SetTexture(textures[newStatus])
+		rc:SetAlpha(1)
 		rc:Show()
-	else		
-		if not timerFrame then
-			timerFrame = CreateFrame("Frame")
-			timerFrame:SetScript('OnUpdate', UpdateAlpha)
+		icons[rc] = nil
+	elseif not newStatus and rc:IsShown() then
+		if oldStatus == 'waiting' then
+			rc:SetTexture(textures.afk)
+			print("=> afking")
 		end
-		icons[rc] = GetTime() + 5
-		timerFrame:Show()
+		print("=> starting fade out timer")
+		icons[rc] = GetTime()+5
+		fadeOutFrame:Show()
 	end
+end
+
+local function OnCheckStart(self, event)
+	--print('ReadyCheck:OnCheckStart', event, self.unit)
+	SetStatus(self, 'waiting')
+end
+
+local function OnCheckAnswer(self, event, unitId, answer)
+	local unit =(GetNumRaidMembers() > 0 and "raid" or "party") .. unitId
+	if UnitIsUnit(unit, self.unit) then
+		--print('ReadyCheck:OnCheckAnswer', event, unit, answer)
+		SetStatus(self, tonumber(answer) == 1 and 'ready' or 'notready')
+	end
+end
+
+local function OnCheckFinished(self)
+	--print('ReadyCheck:OnCheckFinished', self.unit)
+	SetStatus(self, nil)
+end
+
+local function Update(self, event)
+	local status = GetReadyCheckStatus(self.unit)
+	--print('ReadyCheck:Update', event, self.unit, status)
+	SetStatus(self, status)
 end
 
 local function Enable(self)
 	if self.ReadyCheck then
-		self:RegisterEvent('READY_CHECK', Update)
-		self:RegisterEvent('READY_CHECK_CONFIRM', Update)
-		self:RegisterEvent('READY_CHECK_FINISHED', Update)
+		self:RegisterEvent('READY_CHECK', OnCheckStart)
+		self:RegisterEvent('READY_CHECK_CONFIRM', OnCheckAnswer)
+		self:RegisterEvent('READY_CHECK_FINISHED', OnCheckFinished)
+		if not fadeOutFrame then
+			fadeOutFrame = CreateFrame("Frame")
+			fadeOutFrame:SetScript('OnUpdate', FadeOutIcons)
+			fadeOutFrame:Hide()
+		end
 		return true
 	end
 end
 
 local function Disable(self)
 	local rc = self.ReadyCheck
-	if rc then	
-		self:UnregisterEvent('READY_CHECK', Update)
-		self:UnregisterEvent('READY_CHECK_CONFIRM', Update)
-		self:UnregisterEvent('READY_CHECK_FINISHED', Update)
+	if rc then
+		self:UnregisterEvent('READY_CHECK', OnCheckStart)
+		self:UnregisterEvent('READY_CHECK_CONFIRM', OnCheckAnswer)
+		self:UnregisterEvent('READY_CHECK_FINISHED', OnCheckFinished)
 		rc:Hide()
 	end
 end
 
 oUF:AddElement('ReadyCheck', Update, Enable, Disable)
-
