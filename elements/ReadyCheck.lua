c45cc5b3f9d2dfd8361bfef774301f7c53423ed4
Adirelle <adirelle@gmail.com>
2009-09-10 00:42:19 +0200

    Finally fixed that ReadyCheck issue.

--- elements/ReadyCheck.lua
+++ elements/ReadyCheck.lua
@@ -15,70 +15,62 @@ local textures = {
 	afk      = READY_CHECK_AFK_TEXTURE
 }
 
-local icons = {}
-local fadeOutFrame
+local function FadeOut(self, elapsed)
+	local delay = self.delay - elapsed
+	if delay <= 0 then
+		self:Hide()
+	else
+		self.icon:SetAlpha(math.min(delay, 1))
+		self.delay = delay
+	end
+end
 
-local function FadeOutIcons()
-	local now = GetTime()
-	for icon, expireTime in pairs(icons) do
-		local alpha = math.min(expireTime - now, 1)
-		if alpha <= 0 then
-			icon:Hide()
-			icons[icon] = nil
-		else
-			icon:SetAlpha(alpha)
-		end
-	end
-	if not next(icons) then
-		fadeOutFrame:Hide()
-	end
+local function StopFadingOut(self)
+	self.icon:SetAlpha(1)
+	self:SetScript('OnUpdate', nil)
+	self:SetScript('OnHide', nil)
+end
+
+local function StartFadingOut(self, delay)
+	self.delay = delay
+	self:SetScript('OnUpdate', FadeOut)
+	self:SetScript('OnHide', StopFadingOut)
 end
 
 local function SetStatus(self, newStatus)
 	local rc = self.ReadyCheck
 	local oldStatus = rc.status
 	if oldStatus == newStatus then return end
-	--print('ReadyCheck:SetStatus', self.unit, ':', oldStatus, '=>', newStatus)
 	rc.status = newStatus
 	if newStatus == 'waiting' or newStatus == 'ready' or newStatus == 'notready' then
-		--print('=> showing texture', textures[newStatus])
-		rc:SetTexture(textures[newStatus])
-		rc:SetAlpha(1)
+		rc.icon:SetTexture(textures[newStatus])
+		rc.icon:SetAlpha(1)
 		rc:Show()
-		icons[rc] = nil
 	elseif not newStatus and rc:IsShown() then
 		if oldStatus == 'waiting' then
-			rc:SetTexture(textures.afk)
-			--print("=> afking")
+			rc.icon:SetTexture(textures.afk)
 		end
-		--print("=> starting fade out timer")
-		icons[rc] = GetTime()+5
-		fadeOutFrame:Show()
+		StartFadingOut(rc, 5)
 	end
 end
 
-local function OnCheckStart(self, event)
-	--print('ReadyCheck:OnCheckStart', event, self.unit)
-	SetStatus(self, 'waiting')
+local function OnCheckStart(self, ...)
+	SetStatus(self, UnitIsUnit(self.unit, 'player') and 'ready' or 'waiting')
 end
 
 local function OnCheckAnswer(self, event, unitId, answer)
 	local unit =(GetNumRaidMembers() > 0 and "raid" or "party") .. unitId
 	if UnitIsUnit(unit, self.unit) then
-		--print('ReadyCheck:OnCheckAnswer', event, unit, answer)
 		SetStatus(self, tonumber(answer) == 1 and 'ready' or 'notready')
 	end
 end
 
 local function OnCheckFinished(self)
-	--print('ReadyCheck:OnCheckFinished', self.unit)
 	SetStatus(self, nil)
 end
 
 local function Update(self, event)
-	local status = GetReadyCheckStatus(self.unit)
-	--print('ReadyCheck:Update', event, self.unit, status)
-	SetStatus(self, status)
+	SetStatus(self, GetReadyCheckStatus(self.unit))
 end
 
 local function Enable(self)
@@ -86,11 +78,6 @@ local function Enable(self)
 		self:RegisterEvent('READY_CHECK', OnCheckStart)
 		self:RegisterEvent('READY_CHECK_CONFIRM', OnCheckAnswer)
 		self:RegisterEvent('READY_CHECK_FINISHED', OnCheckFinished)
-		if not fadeOutFrame then
-			fadeOutFrame = CreateFrame("Frame")
-			fadeOutFrame:SetScript('OnUpdate', FadeOutIcons)
-			fadeOutFrame:Hide()
-		end
 		return true
 	end
 end
