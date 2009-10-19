9b7016924546fe65575f815bbcafe8a72bde834a
Adirelle <adirelle@tagada-team.net>
2009-10-20 00:27:23 +0200

    Made single layout a module.

--- Raid_Layout.lua
+++ Raid_Layout.lua
@@ -179,23 +179,3 @@ updateFrame:RegisterEvent('VARIABLES_LOADED')
 updateFrame:RegisterEvent('PLAYER_REGEN_ENABLED')
 updateFrame:RegisterEvent('PLAYER_ENTERING_WORLD')
 
----- Single UF
-
-oUF:SetActiveStyle("Adirelle_Single")
-local player = oUF:Spawn("player", "oUF_Adirelle_Player")
-local pet = oUF:Spawn("pet","oUF_Adirelle_Pet")
-
-oUF:SetActiveStyle("Adirelle_Single_Right")
-local target = oUF:Spawn("target", "oUF_Adirelle_Target")
-local focus = oUF:Spawn("focus","oUF_Adirelle_Focus")
-
-oUF:SetActiveStyle("Adirelle_Single_Health")
-local tot = oUF:Spawn("targettarget", "oUF_Adirelle_ToT")
-
-player:SetPoint('BOTTOMLEFT', UIParent, "BOTTOMLEFT", 325, 400)
-pet:SetPoint('BOTTOMLEFT', player, "TOPLEFT", 0, 10)
-
-target:SetPoint('BOTTOMRIGHT', UIParent, "BOTTOMRIGHT", -325, 400)
-tot:SetPoint('BOTTOMLEFT', target, "TOPLEFT", 0, 10)
-focus:SetPoint('BOTTOMLEFT', tot, "TOPLEFT", 0, 10)
-
