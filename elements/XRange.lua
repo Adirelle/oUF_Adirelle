d451aba63618fcc50057343c687e632ae577fe98
Adirelle <adirelle@gmail.com>
2009-10-21 14:04:58 +0200

    XRange enhancements: * fade more out of view units, * check resurrection spells on friendly corpses.

--- elements/XRange.lua
+++ elements/XRange.lua
@@ -6,25 +6,38 @@ All rights reserved.
 
 local oUF = assert(_G.oUF, "oUF_Adirelle requires oUF")
 
+local UnitIsUnit = UnitIsUnit
+local IsSpellInRange = IsSpellInRange
+local UnitIsConnected = UnitIsConnected
+local UnitCanAttack = UnitCanAttack
+local UnitCanAssist = UnitCanAssist
+local UnitInRange = UnitInRange
+local UnitIsCorpse = UnitIsCorpse
+local CheckInteractDistance = CheckInteractDistance
+local GetSpellInfo = GetSpellInfo
+
 local objects = {}
 local updateFrame
 local timer = 0
 
-local friendlySpell, hostileSpell, petSpell
+local friendlySpell, hostileSpell, petSpell, rezSpell
 
 -- Based on list from ShadowedUnitFrames, thanks to Shadowed
 local playerClass = select(2, UnitClass("player"))
 if playerClass == 'PRIEST' then
 	friendlySpell = GetSpellInfo(2050) -- Lesser Heal
 	hostileSpell = GetSpellInfo(48127) -- Mind Blast
+	rezSpell = GetSpellInfo(2006) -- Resurrection
 	
 elseif playerClass == 'DRUID' then
 	friendlySpell = GetSpellInfo(48378) -- Healing Touch 
 	hostileSpell = GetSpellInfo(48461) -- Wrath
+	rezSpell = GetSpellInfo(20484) -- Rebirth
 	
 elseif playerClass == 'PALADIN' then
 	friendlySpell = GetSpellInfo(48782) -- Holy Light
 	hostileSpell = GetSpellInfo(62124) -- Hand of Reckoning
+	rezSpell = GetSpellInfo(7328) -- Redemption
 	
 elseif playerClass == 'HUNTER' then
 	hostileSpell = GetSpellInfo(75) -- Auto Shot
@@ -33,6 +46,7 @@ elseif playerClass == 'HUNTER' then
 elseif playerClass == 'SHAMAN' then
 	friendlySpell = GetSpellInfo(49273) -- Healing Wave
 	hostileSpell = GetSpellInfo(529) -- Lightning Bolt
+	rezSpell = GetSpellInfo(2008) -- Ancestral Spirit
 	
 elseif playerClass == 'WARLOCK' then
 	hostileSpell = GetSpellInfo(686) -- Shadow Bolt
@@ -46,38 +60,34 @@ elseif playerClass == 'DEATHKNIGHT' then
 	hostileSpell = GetSpellInfo(49576) -- Death grip
 end
 
-local UnitIsUnit = UnitIsUnit
-local IsSpellInRange = IsSpellInRange
-local UnitCanAttack = UnitCanAttack
-local UnitCanAssist = UnitCanAssist
-local UnitInRange = UnitInRange
-local CheckInteractDistance = CheckInteractDistance
-
-local function InRange(unit)
-	if UnitIsUnit(unit, 'player') then return true end
-	local spellInRange
-	if petSpell and UnitIsUnit(unit, 'pet') then
-		spellInRange = IsSpellInRange(petSpell, unit)	
-	elseif hostileSpell and UnitCanAttack('player', unit) then
-		spellInRange = IsSpellInRange(hostileSpell, unit)
-	elseif friendlySpell and UnitCanAssist('player', unit) then
-		spellInRange = IsSpellInRange(friendlySpell, unit)
+local function GetRangeSpell(unit)
+	if UnitCanAssist('player', unit) then
+		return (UnitIsCorpse(unit) and rezSpell) or (UnitIsUnit(unit, 'pet') and petSpell) or friendlySpell
+	elseif UnitCanAttack('player', unit) then
+		return hostileSpell
 	end
-	if spellInRange ~= nil then
-		return spellInRange == 1
+end
+
+local function GetRangeAlpha(self, unit)
+	if UnitIsUnit(unit, 'player') or not UnitIsConnected(unit) then 
+		return self.inRangeAlpha
+	elseif not UnitIsVisible(unit) then
+		return self.notVisibleAlpha
+	end
+	local spell =  GetRangeSpell(unit)
+	local spellInRange = spell and IsSpellInRange(spell, unit)
+	if spellInRange == 1 or (spellInRange == nil and (UnitInRange(unit) or CheckInteractDistance(unit, 4))) then
+		return self.inRangeAlpha
 	else
-		return UnitInRange(unit) or CheckInteractDistance(unit, 4)
+		return self.outsideRangeAlpha
 	end
 end
 
 local function Update(self, event, unit)
 	if unit and unit ~= self.unit then return end
-	if UnitIsConnected(unit) and not InRange(unit) then
-		if self:GetAlpha() == self.inRangeAlpha then
-			self:SetAlpha(self.outsideRangeAlpha)
-		end
-	elseif self:GetAlpha() ~= self.inRangeAlpha then
-		self:SetAlpha(self.inRangeAlpha)
+	local alpha = GetRangeAlpha(self, unit)
+	if alpha ~= self:GetAlpha() then
+		self:SetAlpha(alpha)
 	end
 end
 
@@ -106,6 +116,7 @@ local function Enable(self)
 		updateFrame:Show()
 		self.inRangeAlpha = self.inRangeAlpha or 1.0
 		self.outsideRangeAlpha = self.outsideRangeAlpha or 0.4
+		self.notVisibleAlpha = notVisibleAlpha or 0.2
 		objects[self] = true
 		return true
 	end
