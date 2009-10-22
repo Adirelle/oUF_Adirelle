a782e3f4cc995368044cb02b761525d7edbd1dfc
Adirelle <adirelle@gmail.com>
2009-10-22 15:42:48 +0200

    Commented dragons.

--- Single_Style.lua
+++ Single_Style.lua
@@ -510,6 +510,31 @@ local function InitFrame(settings, self)
 		self.PostCreateAuraIcon = PostCreateAuraIcon
 	end
 	
+	-- Elite/rare status
+	--[[
+	if unit == "target" or unit == "focus" then
+		local textures = { rate = 'rare', rareelite = 'rare', elite = 'elite', worldboss = 'elite', boss = 'elite' }
+		local dragon = indicators:CreateTexture(nil, "OVERLAY")
+		dragon:SetHeight(70)
+		dragon:SetWidth(70)
+		dragon:SetPoint("CENTER", self, "TOPLEFT", 8, -18)
+		self.Dragon = dragon		
+		local function UpdateDragon(self)
+			local dragon = self.Dragon
+			local texture = textures[UnitClassification(self.unit) or false]
+			if texture then
+				dragon:SetTexture([[Interface\\AddOns\\TipTop\\media\\]]..texture..[[_graphic]])
+				dragon:Show()
+			else
+				dragon:Hide()
+			end
+		end
+		local function Enable() end
+		self:RegisterEvent('UNIT_CLASSIFICATION_CHANGED', UpdateDragon)
+		self:RegisterEvent('PLAYER_ENTERING_WORLD', UpdateDragon)
+	end
+	--]]
+	
 	-- Range fading
 	self.XRange = true
 	
