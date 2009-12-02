dc8c4777b0b9f046c27114929cec659e3b5f8568
Adirelle <adirelle@gmail.com>
2009-12-02 12:03:51 +0100

    Fixed DK's rune bar, really. Moved rune bar code in its own file.

--- Single_Style.lua
+++ Single_Style.lua
@@ -179,91 +179,30 @@ local _, playerClass = UnitClass('player')
 
 local SetupAltPower
 if playerClass == 'DEATHKNIGHT' then
+	
+	local function LayoutRunes(runeBar)
+		local spacing = (runeBar:GetWidth() + GAP) / 6
+		local runeWidth = spacing - GAP
+		local runeHeight = runeBar:GetHeight()
+		for index = 1, 6 do
+			local rune = runeBar[index]
+			rune:SetPoint("TOPLEFT", runeBar, "TOPLEFT", spacing * (index-1), 0)
+			rune:SetWidth(runeWidth)
+			rune:SetHeight(runeHeight)
+		end
+	end
+		
 	function SetupAltPower(self)
-	
-		oUF.colors.runes = oUF.colors.runes or {
-			{ 1, 0, 0  },
-			{ 0, 0.5, 0 },
-			{ 0, 1, 1 },
-			{ 0.8, 0.1, 1 },
-		}
-
-		local function UpdateRuneColor(rune)
-			local color = oUF.colors.runes[GetRuneType(rune.index) or false]
-			if color then
-				rune:SetStatusBarColor(unpack(color))
-			end
-		end
-	
-		local GetTime = GetTime
-		local function OnUpdate(rune)
-			local now = GetTime()
-			rune:SetValue(now)
-			if now > rune.readyTime then
-				rune:SetScript('OnUpdate', nil)
-			end
-		end
-
-		local function RuneUpdate(self, event, index)
-			local rune = self.RuneBar[index]
-			if not rune then return end
-			UpdateRuneColor(rune)
-			local start, duration, ready = GetRuneCooldown(index)
-			if not ready then
-				rune.duration = duration
-				rune.readyTime = start + duration
-				rune:SetMinMaxValues(start, start + duration)
-				rune:SetScript('OnUpdate', OnUpdate)
-			else
-				rune:SetScript('OnUpdate', nil)		
-			end
-		end
-	
-		local function Update(self, event, index)
-			if event == 'PLAYER_ENTERING_WORLD' then
-				if self.unit ~= 'player' then
-					return self.RuneBar:Hide()
-				else
-					self.RuneBar:Show()
-				end
-				for index = 1, 6 do 
-					RuneUpdate(self, event, index) 
-				end
-			else
-				RuneUpdate(self, event, index) 
-			end		
-		end
-	
-		local function LayoutRunes(runeBar)
-			local spacing = (runeBar:GetWidth() + GAP) / 6
-			local runeWidth = spacing - GAP
-			local runeHeight = runeBar:GetHeight()
-			for index = 1, 6 do
-				local rune = runeBar[index]
-				rune:SetPoint("TOPLEFT", runeBar, "TOPLEFT", spacing * (index-1), 0)
-				rune:SetWidth(runeWidth)
-				rune:SetHeight(runeHeight)
-			end
-		end
-
 		local runeBar = CreateFrame("Frame", nil, self)
+		runeBar:HookScript('OnShow', LayoutRunes)
 		runeBar:HookScript('OnSizeChanged', LayoutRunes)
 		self.RuneBar = runeBar
-		
 		for index = 1, 6 do
 			local rune = CreateFrame("StatusBar", nil, runeBar)
 			rune.index = index
 			self:RegisterStatusBarTexture(rune, UpdateRuneColor)
 			runeBar[index] = rune
 		end
-
-		self:RegisterEvent('RUNE_POWER_UPDATE', Update)
-		self:RegisterEvent('RUNE_TYPE_UPDATE', Update)
-
-		RuneFrame:Hide()
-		RuneFrame.Show = RuneFrame.Hide
-		RuneFrame:UnregisterAllEvents()
-		
 		return runeBar
 	end
 
