4a0ca2f525567b8e3116c68d7c0ed6b017ab84aa
Adirelle <adirelle@tagada-team.net>
2010-01-13 14:21:05 +0100

    Fixed TotemBar and moved it into its own file.

---
+++ elements/TotemBar.lua
@@ -0,0 +1,54 @@
+--[=[
+Adirelle's oUF layout
+(c) 2009 Adirelle (adirelle@tagada-team.net)
+All rights reserved.
+
+Elements handled: .RuneBar
+--]=]
+
+local parent, ns = ...
+local oUF = assert(ns.oUF, "oUF is undefined in "..parent.." namespace")
+
+local function OnUpdate(totem, elapsed)			
+	local timeLeft = totem:GetValue() - elapsed
+	if timeLeft <= 0 then
+		totem:Hide()
+	else
+		totem:SetValue(timeLeft)
+	end
+end
+
+local function Update(self, event, ...)
+	self:Debug('TotemBar:Update', event, ...)
+	for index, totem in ipairs(self.TotemBar) do
+		local haveTotem, name, start, duration = GetTotemInfo(totem.totemType)
+		self:Debug('TotemBar', index, totem, haveTotem, name, start, duration)
+		if haveTotem and name and name ~= "" then
+			totem:SetMinMaxValues(0, duration)
+			totem:SetValue(start+duration-GetTime())
+			totem:Show()
+		else
+			totem:Hide()
+		end
+	end
+end
+
+local function Enable(self)
+	if self.TotemBar then
+		self:RegisterEvent('PLAYER_TOTEM_UPDATE', Update)
+		for index, totem in ipairs(self.TotemBar) do
+			totem:Hide()
+			totem:SetScript('OnUpdate', OnUpdate)
+		end
+		self.TotemBar:Show()
+		return true
+	end
+end
+
+local function Disable(self)
+	if self.TotemBar then
+		self:UnregisterEvent('PLAYER_TOTEM_UPDATE', Update)
+	end
+end
+
+oUF:AddElement('TotemBar', Update, Enable, Disable)
