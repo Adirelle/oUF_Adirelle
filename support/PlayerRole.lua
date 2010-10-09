79c8334ae4b2462bcc0f624b4cf0e429ed70fd7b
Adirelle <adirelle@gmail.com>
2010-10-09 10:31:49 +0200

    Use the new Cata API to get the player role.

--- support/PlayerRole.lua
+++ support/PlayerRole.lua
@@ -8,31 +8,46 @@ All rights reserved.
 local _G, parent, ns = _G, ...
 setfenv(1, ns)
 
--- FIXME
-function GetPlayerRole() 
-	return "unknown"
-end
-function RegisterPlayerRoleCallback(callback)
+-- DOC:
+-- talentGroup = GetActiveTalentGroup(inspect, pet)
+-- talentTree = GetPrimaryTalentTree(inspect, pet, talentGroup)
+-- role1, role2 = GetTalentTreeRoles(talentTree, inspect, pet)
+
+function GetPlayerRole()
+	local talentGroup = GetActiveTalentGroup(false,false)
+	if not talentGroup then return end
+	local primaryTree = GetPrimaryTalentTree(false, false, talentGroup)
+	if not primaryTree then return end
+	local role1, role2 = GetTalentTreeRoles(primaryTree, false, false)
+	if role1 == "HEALER" or role2 == "HEALER" then
+		return "healer"
+	elseif role1 == "TANK" or role2 == "TANK" then
+		return "tank"
+	else
+		return "melee"
+	end
 end
 
---[[
-local GuessRole = GetLib('LibGuessRole-1.0')
+local frame, callbacks, current
 
-local roleMap = {
-	[GuessRole.ROLE_MELEE] = 'damager',
-	[GuessRole.ROLE_CASTER] = 'damager',
-	[GuessRole.ROLE_TANK] = 'tank',
-	[GuessRole.ROLE_HEALER] = 'healer',
-}
-function GetPlayerRole() 
-	return roleMap[GuessRole:GetUnitRole('player') or false] or "unknown"
-end
-function RegisterPlayerRoleCallback(callback)
-	GuessRole.RegisterCallback(tostring(callback), "LibGuessRole_RoleChanged", function(event, guid)
-		if UnitGUID("player") == guid then
-			return callback()	
+local function OnEvent()
+	local role = GetPlayerRole()
+	if role ~= current then
+		current = role
+		for callback in pairs(callbacks) do
+			pcall(callback)
 		end
-	end)
+	end
 end
---]]
+
+function RegisterPlayerRoleCallback(callback)
+	if not frame then
+		frame, callbacks = CreateFrame("Frame"), {}
+		frame:SetScript('OnEvent', OnEvent)
+		frame:RegisterEvent("PLAYER_TALENT_UPDATE")
+		frame:RegisterEvent("ACTIVE_TALENT_GROUP_CHANGED")
+		frame:RegisterEvent("PLAYER_ALIVE")
+	end
+	callbacks[callback] = true
+end
 
