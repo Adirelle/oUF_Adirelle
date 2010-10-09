84299e820a394b66f8b88b30a323b6395a36b3b0
Adirelle <adirelle@gmail.com>
2010-10-09 10:32:19 +0200

    Use the new Cata API to getting party/raid member roles.

--- elements/RoleOrIcon.lua
+++ elements/RoleOrIcon.lua
@@ -13,12 +13,10 @@ local Debug = ns.Debug
 
 local GetRaidTargetIndex = GetRaidTargetIndex
 local SetRaidTargetIconTexture = SetRaidTargetIconTexture
-local GetPartyAssignment = GetPartyAssignment
 local UnitInRaid = UnitInRaid
-local UnitInParty = UnitInParty
+local UnitIsPlayer = UnitIsPlayer
 local UnitGroupRolesAssigned = UnitGroupRolesAssigned
 local GetRealNumRaidMembers = GetRealNumRaidMembers
-local GetRealNumPartyMembers = GetRealNumPartyMembers
 
 local LibGuessRole = ns.GetLib('LibGuessRole-1.0')
 
@@ -29,40 +27,33 @@ local function GetRole(unit, noDamager)
 	end
 
 	-- Check assigned raid roles
-	if GetRealNumRaidMembers() > 0 and UnitInRaid(unit) then
-		Debug('GetPartyAssignment for ', unit, ': MAINASSIST=', GetPartyAssignment("MAINASSIST", unit), "MAINTANK=", GetPartyAssignment("MAINTANK", unit))
-		if GetPartyAssignment("MAINASSIST", unit) then
-			return [[Interface\GroupFrame\UI-Group-MainAssistIcon]]
-		elseif GetPartyAssignment("MAINTANK", unit) then
-			return [[Interface\GroupFrame\UI-Group-MainTankIcon]]
+	local raidId = GetRealNumRaidMembers() > 0 and UnitInRaid(unit)
+	if raidId then
+		local role = select(10, GetRaidRosterInfo(raidId))
+		if role and role ~= "NONE" and (not noDamager or role ~= "DAMAGER") then
+			return "Interface\\GroupFrame\\UI-Group-"..role.."Icon"
 		end
+	end
 
-	-- Check assigned LFD roles
-	elseif GetRealNumPartyMembers() > 0 and UnitInParty(unit) then
-		local isTank, isHealer, isDamager = UnitGroupRolesAssigned(unit)
-		Debug('UnitGroupRolesAssigned', unit, ':', isTank and "tank", isHealer and "healer", isDamager and "damager")
-		if isTank then
-			return [[Interface\LFGFrame\LFGRole]], 0.5, 0.75
-		elseif isHealer then
-			return [[Interface\LFGFrame\LFGRole]], 0.75, 1
-		elseif isDamager then
-			if noDamager then
-				return
-			else
-				return [[Interface\LFGFrame\LFGRole]], 0.25, 0.5
-			end
+	-- Check assigned roles
+	local role = UnitGroupRolesAssigned(unit)
+	Debug('UnitGroupRolesAssigned', unit, ':', role)
+	if role and role ~= "NONE" then
+		if noDamager and role == "DAMAGER" then
+			return
 		end
+		return [[Interface\LFGFrame\UI-LFG-ICON-PORTRAITROLES]], GetTexCoordsForRoleSmallCircle(role)
 	end
 
 	-- Fallback on LibGuessRole
 	local role, level = LibGuessRole:GetUnitRole(unit)
 	Debug('LibGuessRole:GetUnitRole for', unit, ':', role, level)
 	if role == LibGuessRole.ROLE_TANK then
-		return [[Interface\LFGFrame\LFGRole_BW]], 0.5, 0.75, 1, 0.82, 0
+		return [[Interface\LFGFrame\LFGRole_BW]], 0.5, 0.75, 0, 1, 1, 0.82, 0
 	elseif role == LibGuessRole.ROLE_HEALER then
-		return [[Interface\LFGFrame\LFGRole_BW]], 0.75, 1, 1, 0.82, 0
+		return [[Interface\LFGFrame\LFGRole_BW]], 0.75, 1, 0, 1, 1, 0.82, 0
 	elseif role and not noDamager then
-		return [[Interface\LFGFrame\LFGRole_BW]], 0.25, 0.5, 1, 0.82, 0
+		return [[Interface\LFGFrame\LFGRole_BW]], 0.25, 0.5, 0, 1, 1, 0.82, 0
 	end
 end
 
@@ -80,29 +71,38 @@ local function Update(self, event, unit)
 		return icon:Show()
 	end
 
-	local texture, x0, x1, r, g, b = GetRole(unit, icon.noDamager)
+	-- Check role
+	local texture, x0, x1, y0, y1 r, g, b = GetRole(unit, icon.noDamager)
 	if texture then
 		icon:SetTexture(texture)
 		icon:SetVertexColor(r or 1, g or 1, b or 1, 1)
-		icon:SetTexCoord(x0 or 0, x1 or 1, 0, 1)
+		icon:SetTexCoord(x0 or 0, x1 or 1, y0 or 0, y1 or 1)
 		icon:Show()
 	else
 		icon:Hide()
 	end
 end
 
+local function Path(self, ...)
+	return (self.RoleIcon.Update or Update)(self, ...)
+end
+
+local function ForceUpdate(element)
+	return Path(element.__owner, 'ForceUpdate')
+end
+
 local function RoleChanged(self, event, guid)
 	if self.unit and UnitGUID(self.unit) == guid then
-		return Update(self, event)
+		return Path(self, event)
 	end
 end
 
 local function Enable(self)
 	if self.RoleIcon then
-		self:RegisterEvent('PARTY_MEMBERS_CHANGED', Update)
-		self:RegisterEvent("RAID_TARGET_UPDATE", Update)
-		self:RegisterEvent('RAID_ROSTER_UPDATE', Update)
-		self:RegisterEvent('LFG_ROLE_UPDATE', Update)
+		self:RegisterEvent('PARTY_MEMBERS_CHANGED', Path)
+		self:RegisterEvent("RAID_TARGET_UPDATE", Path)
+		self:RegisterEvent('RAID_ROSTER_UPDATE', Path)
+		self:RegisterEvent('PLAYER_ROLES_ASSIGNED', Path)
 		LibGuessRole.RegisterCallback(self, "LibGuessRole_RoleChanged", RoleChanged, self)
 		self.RoleIcon:Hide()
 		return true
@@ -110,13 +110,15 @@ local function Enable(self)
 end
 
 local function Disable(self)
-	if self.RoleIcon then
-		self:UnregisterEvent('PARTY_MEMBERS_CHANGED', Update)
-		self:UnregisterEvent("RAID_TARGET_UPDATE", Update)
-		self:UnregisterEvent('LFG_ROLE_UPDATE', Update)
-		self:UnregisterEvent('RAID_ROSTER_UPDATE', Update)
+	local icon = self.RoleIcon
+	if icon then
+		icon.__owner, icon.ForceUpdate = self, ForceUpdate
+		self:UnregisterEvent('PARTY_MEMBERS_CHANGED', Path)
+		self:UnregisterEvent("RAID_TARGET_UPDATE", Path)
+		self:UnregisterEvent('LFG_ROLE_UPDATE', Path)
+		self:UnregisterEvent('PLAYER_ROLES_ASSIGNED', Path)
 		LibGuessRole.UnregisterCallback(self, "LibGuessRole_RoleChanged")
-		self.RoleIcon:Hide()
+		icon:Hide()
 	end
 end
 
