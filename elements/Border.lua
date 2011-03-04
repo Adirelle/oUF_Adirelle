43e042618fa555f7f49508c14b32760025dd03cc
Adirelle <adirelle@tagada-team.net>
2011-03-04 16:41:24 +0100

    Slightly optimized the Border element.

--- elements/Border.lua
+++ elements/Border.lua
@@ -25,10 +25,8 @@ local function Update(self, event, unit)
 	if unit and UnitExists(unit) then
 		if not border.noTarget and UnitIsUnit('target', unit) then
 			r, g, b = 1, 1, 1
-		elseif not UnitIsDeadOrGhost(unit) then
-			if UnitPowerType(unit) == SPELL_POWER_MANA and UnitPower(unit, SPELL_POWER_MANA) / UnitPowerMax(unit, SPELL_POWER_MANA) < 0.25 then
-				r, g, b = unpack(oUF.colors.power.MANA)
-			end
+		elseif not UnitIsDeadOrGhost(unit) and UnitPowerType(unit) == SPELL_POWER_MANA and UnitPower(unit, SPELL_POWER_MANA) / UnitPowerMax(unit, SPELL_POWER_MANA) < 0.25 then
+			r, g, b = unpack(oUF.colors.power.MANA)
 		end
 	end
 	if b then
@@ -37,6 +35,7 @@ local function Update(self, event, unit)
 	else
 		border:Hide()
 	end
+	return true
 end
 
 local function PowerUpdate(self, event, unit, power)
@@ -45,10 +44,28 @@ local function PowerUpdate(self, event, unit, power)
 	end
 end
 
+local function TogglePowerUpdates(self, event, unit)
+	if not unit or unit == self.unit then
+		if UnitPowerType(unit) == SPELL_POWER_MANA then
+			self:RegisterEvent("UNIT_POWER", PowerUpdate)
+			self:RegisterEvent("UNIT_MAXPOWER", PowerUpdate)
+		else
+			self:UnregisterEvent("UNIT_POWER", PowerUpdate)
+			self:UnregisterEvent("UNIT_MAXPOWER", PowerUpdate)
+		end
+		return Update(self, event, unit)
+	end
+end
+
+local function FullUpdate(self, event)
+	if not TogglePowerUpdates(self, event, self.unit) then
+		return Update(self, event, self.unit)
+	end
+end
+
 local function Enable(self)
 	if self.Border then
-		self:RegisterEvent("UNIT_POWER", PowerUpdate)
-		self:RegisterEvent("UNIT_MAXPOWER", PowerUpdate)
+		self:RegisterEvent("UNIT_DISPLAYPOWER", TogglePowerUpdates)
 		self:RegisterEvent("PLAYER_TARGET_CHANGED", Update)			
 		self.Border:Hide()
 		return true
@@ -60,10 +77,11 @@ local function Disable(self)
 	if border then
 		self:UnregisterEvent("UNIT_POWER", PowerUpdate)
 		self:UnregisterEvent("UNIT_MAXPOWER", PowerUpdate)
+		self:UnregisterEvent("UNIT_DISPLAYPOWER", TogglePowerUpdates)
 		self:UnregisterEvent("PLAYER_TARGET_CHANGED", Update)			
 		border:Hide()
 	end
 end
 
-oUF:AddElement('Border', Update, Enable, Disable)
+oUF:AddElement('Border', FullUpdate, Enable, Disable)
 
