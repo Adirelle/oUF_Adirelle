142c78ab5265ba25248aa34e487ed0ed6505bc36
Adirelle <adirelle@gmail.com>
2011-04-01 10:56:50 +0200

    Do not hide arena frames until the plyayer leaves the arena so they are kept visible if the unit enters stealth (drycoded, need testing).

--- modules/arena/Layout.lua
+++ modules/arena/Layout.lua
@@ -8,24 +8,64 @@ All rights reserved.
 setfenv(1, _G.oUF_Adirelle)
 
 oUF:Factory(function()
+
+	local function Enable(self)
+		RegisterUnitWatch(self, true)
+	end
+	
+	local function PLAYER_ENTERING_WORLD(self)
+		if select(2, IsInInstance()) == "arena" and (not self.LM10_IsEnabled or self:LM10_IsEnabled()) then
+			self:Enable()
+		else
+			self:Disable()
+		end
+	end
+
+	local function Spawn(unit, label, ...)
+		-- Spawn the frame
+		local frame = oUF:Spawn(unit:lower(), "oUF_Adirelle_"..unit)
+
+		-- Position it and make it movable
+		frame:SetPoint(...)
+		RegisterMovable(frame, unit, label)
+
+		-- Show the frame when the unit comes to existence, never hide, always update
+		SecureHandlerWrapScript(frame, "OnAttributeChanged", frame, [=[
+			if name == "state-unitexists" then
+				if value then
+					control:CallMethod("Debug", "unit appeared")
+					if not self:IsShown() then
+						self:Show() 
+					else
+						control:CallMethod("UpdateAllElements", "OnUnitExists")
+					end
+				else
+					control:CallMethod("Debug", "unit disappeared")
+				end
+			end
+		]=])
+
+		-- Use PEW to enable/disable the frame, using our own made :Enable
+		frame.Enable = Enable
+		frame:RegisterEvent('PLAYER_ENTERING_WORLD', PLAYER_ENTERING_WORLD)
+		
+		-- Call it at least once, in case of LoD
+		PLAYER_ENTERING_WORLD(frame, 'OnSpawn')
+
+		return frame
+	end
+
 	oUF:SetActiveStyle("Adirelle_Single_Right")
 	local anchor, gap = oUF_Adirelle_Focus, 30
 	for index = 1, 5 do
-		local unit = "Arena"..index
-		local frame = oUF:Spawn(unit:lower(), "oUF_Adirelle_"..unit)
-		frame:SetPoint("BOTTOM", anchor, "TOP", 0, gap)
-		RegisterMovable(frame, unit, format("Arena enemy #%d", index))
-		anchor, gap = frame, 40
+		anchor, gap = Spawn("Arena"..index, format("Arena enemy #%d", index), "BOTTOM", anchor, "TOP", 0, gap), 40
 	end
 
 	oUF:SetActiveStyle("Adirelle_Single_Health")
 	for index = 1, 5 do
-		local unit = "ArenaPet"..index
-		local frame = oUF:Spawn(unit:lower(), "oUF_Adirelle_"..unit)
-		frame:SetPoint("BOTTOM", _G["oUF_Adirelle_Arena"..index], "TOP", 0, 5)
-		RegisterMovable(frame, unit, format("Arena enemy pet #%d", index))
+		Spawn("ArenaPet"..index, format("Arena enemy pet #%d", index), "BOTTOM", _G["oUF_Adirelle_Arena"..index], "TOP", 0, 5)
 	end
-	
+
 	-- Prevent loading of Blizzard arena frames
 	_G.Arena_LoadUI = function() end
 	if _G.ArenaEnemyFrames then
