d7b6bc9f1f490e1c9f1549cda7a91803fedafd1b
Adirelle <adirelle@gmail.com>
2011-04-04 11:02:38 +0200

    Put all boss and arena frames into two container frames.

--- modules/arena/Layout.lua
+++ modules/arena/Layout.lua
@@ -4,85 +4,83 @@ Adirelle's oUF layout
 All rights reserved.
 --]=]
 
--- Use our own namespace
-setfenv(1, _G.oUF_Adirelle)
-
-oUF:Factory(function()
-
+oUF_Adirelle.oUF:Factory(function()
+	local oUF, RegisterMovable = oUF_Adirelle.oUF, oUF_Adirelle.RegisterMovable
+	
 	local frames = {}
+	
+	local anchor = CreateFrame("Frame", "oUF_Adirelle_Arena", UIParent, "SecureFrameTemplate")
+	anchor.Debug = oUF_Adirelle.Debug
+	anchor:SetSize(190, 5*(47+40)-15)
+	anchor:SetPoint("BOTTOM", oUF_Adirelle_Focus, "TOP", 0, 30)
+	
+	function anchor:Enable()
+		self:Show()
+		for i, frame in pairs(frames) do
+			frame:Enable()
+		end
+	end
 
-	local function Enable(self)
+	function anchor:Disable()
+		for i, frame in pairs(frames) do
+			frame:Disable()
+		end
+		self:Hide()
+	end
+	
+	function anchor:Update()
+		if select(2, IsInInstance()) == "arena" and (not self.LM10_IsEnabled or self:LM10_IsEnabled()) then
+			self:Enable()
+		else
+			self:Disable()
+		end
+	end
+
+	anchor:RegisterEvent('PLAYER_ENTERING_WORLD')
+	anchor:RegisterEvent('ZONE_CHANGED_NEW_AREA')
+	anchor:SetScript('OnEvent', anchor.Update)
+
+	local function ArenaUnit_Enable(self)
 		RegisterUnitWatch(self, true)
 	end
 
-	local function Disable(self)
+	local function ArenaUnit_Disable(self)
 		UnregisterUnitWatch(self)
 		self:SetAttribute('state-unitexists', false)
 		self:Hide()
 	end
-
-	local function Update(self, event)
-		if select(2, IsInInstance()) == "arena" and (not self.LM10_IsEnabled or self:LM10_IsEnabled()) then
-			self:Enable()
-			self:Debug('Enabled on', event)
-		else
-			self:Disable()
-			self:Debug('Disabled on', event)
-		end
-	end
 	
-	local monitor = CreateFrame("Frame")
-	monitor:SetScript('OnEvent', function(self, ...)
-		for frame in pairs(frames) do
-			Update(frame, ...)
-		end
-	end)
-	monitor:RegisterEvent('PLAYER_ENTERING_WORLD')
-	monitor:RegisterEvent('ZONE_CHANGED_NEW_AREA')
-
-	local function Spawn(unit, label, ...)
-		-- Spawn the frame
-		local frame = oUF:Spawn(unit:lower(), "oUF_Adirelle_"..unit)
-
-		-- Position it and make it movable
-		frame:SetPoint(...)
-		RegisterMovable(frame, unit, label)
-
-		-- Show the frame when the unit comes to existence, never hide, always update
-		SecureHandlerWrapScript(frame, "OnAttributeChanged", frame, [=[
-			if name == "state-unitexists" then
-				if value then
-					if not self:IsShown() then
-						self:Show()
-					else
-						control:CallMethod("UpdateAllElements", "OnUnitExists")
-					end
+	local ArenaUnit_OnAttributeChanged = [=[
+		if name == "state-unitexists" then
+			if value then
+				if not self:IsShown() then
+					self:Show()
+				else
+					control:CallMethod("UpdateAllElements", "OnUnitExists")
 				end
 			end
-		]=])
+		end
+	]=]
 
-		-- Put our handlers in place
-		frame.Enable, frame.Disable = Enable, Disable
-
-		-- Update at lease once
-		Update(frame, 'OnSpawn')
-
-		-- Register for future updates
-		frames[frame] = true
-
-		return frame
-	end
-
-	oUF:SetActiveStyle("Adirelle_Single_Right")
-	local anchor, gap = oUF_Adirelle_Focus, 30
 	for index = 1, 5 do
-		anchor, gap = Spawn("Arena"..index, format("Arena enemy #%d", index), "BOTTOM", anchor, "TOP", 0, gap), 40
-	end
+		oUF:SetActiveStyle("Adirelle_Single_Right")
+		local frame = oUF:Spawn("arena"..index, "oUF_Adirelle_Arena"..index)
+		frame:SetParent(anchor)
+		frame:SetPoint("BOTTOM", anchor, "BOTTOM", 0, (index-1) * (40+47))
+		frame.Enable, frame.Disable = ArenaUnit_Enable, ArenaUnit_Disable
+		SecureHandlerWrapScript(frame, "OnAttributeChanged", frame, ArenaUnit_OnAttributeChanged)
+		tinsert(frames, frame)
 
-	oUF:SetActiveStyle("Adirelle_Single_Health")
-	for index = 1, 5 do
-		Spawn("ArenaPet"..index, format("Arena enemy pet #%d", index), "BOTTOM", _G["oUF_Adirelle_Arena"..index], "TOP", 0, 5)
+		oUF:SetActiveStyle("Adirelle_Single_Health")	
+		local petFrame = oUF:Spawn("arenapet"..index, "oUF_Adirelle_ArenaPet"..index)
+		petFrame:SetParent(anchor)
+		petFrame:SetPoint("BOTTOM", frame, "TOP", 0 ,5)
+		tinsert(frames, petFrame)
 	end
+	
+	-- Initialize
+	RegisterMovable(anchor, "arenas", "Arena enemy frames")	
+	anchor:Update()
 
 	-- Prevent loading of Blizzard arena frames
 	_G.Arena_LoadUI = function() end
--- modules/boss/Layout.lua
+++ modules/boss/Layout.lua
@@ -4,17 +4,34 @@ Adirelle's oUF layout
 All rights reserved.
 --]=]
 
--- Use our own namespace
-setfenv(1, _G.oUF_Adirelle)
+oUF_Adirelle.oUF:Factory(function()
+	local oUF, RegisterMovable = oUF_Adirelle.oUF, oUF_Adirelle.RegisterMovable
 
-oUF:Factory(function()
+	local anchor = CreateFrame("Frame", "oUF_Adirelle_Bosses", UIParent, "SecureFrameTemplate")
+	anchor.Debug = oUF_Adirelle.Debug
+	anchor:SetSize(190, 47*4+15*3)
+	anchor:SetPoint("BOTTOM", oUF_Adirelle_Focus, "TOP", 0, 30)
+	
 	oUF:SetActiveStyle("Adirelle_Single_Right")
-	local anchor, gap = oUF_Adirelle_Focus, 30
+	local frames = {}
 	for index = 1, MAX_BOSS_FRAMES do
-		local unit = "Boss"..index
-		local frame = oUF:Spawn(unit:lower(), "oUF_Adirelle_"..unit)
-		frame:SetPoint("BOTTOM", anchor, "TOP", 0, gap)
-		anchor, gap = frame, 15
-		RegisterMovable(frame, unit, unit.." frame")
+		local frame = oUF:Spawn("boss"..index, "oUF_Adirelle_Boss"..index)
+		frame:SetParent(anchor)
+		frame:SetPoint("BOTTOM", anchor, "BOTTOM", 0, (47+15)*(index-1))
+		frames[index] = frame
 	end
+	
+	function anchor:Enable()
+		self:Show()
+		for i, frame in ipairs(frames) do
+			frame:Enable()
+		end
+	end
+	function anchor:Disable()
+		for i, frame in ipairs(frames) do
+			frame:Disable()
+		end
+		self:Hide()
+	end
+	RegisterMovable(anchor, "bosses", "Boss frames")
 end)
