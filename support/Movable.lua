3afd200d36d83740e3adff54d49444e3ec546752
Adirelle <adirelle@gmail.com>
2011-04-24 09:51:53 +0200

    Switched to AceDB-3.0 for SV handling.

--- support/Movable.lua
+++ support/Movable.lua
@@ -8,38 +8,55 @@ local _G, addonName, private = _G, ...
 local oUF_Adirelle, assert = _G.oUF_Adirelle, _G.assert
 local oUF = assert(oUF_Adirelle.oUF, "oUF is undefined in oUF_Adirelle")
 
-local pairs = _G.pairs
+local pairs, unpack = _G.pairs, _G.unpack
 
 local libmovable = oUF_Adirelle.GetLib('LibMovable-1.0')
 if libmovable then
 
-	-- Function used until our SV is loaded
+	local db
+
+	-- Functions used once the settings are loaded	
+	local function LM10_Enable(frame) return frame:SetEnabledSetting(true) end
+	local function LM10_Disable(frame) return frame:SetEnabledSetting(false) end
+	local function DoRegister(frame, key, label, mask)
+		frame:Debug('Registering movable', key, label, mask)
+
+		oUF_Adirelle.RegisterTogglableFrame(frame, key)
+		frame.LM10_IsEnabled = frame.GetEnabledSetting
+		frame.LM10_Enable = LM10_Enable
+		frame.LM10_Disable = LM10_Disable
+		
+		libmovable.RegisterMovable(addonName, frame, db[key], label, mask)
+	end
+
+	-- Function used until the settings are loaded
 	local postponed = {}
 	oUF_Adirelle.RegisterMovable = function(frame, key, label, mask)
-		postponed[frame] = function() return oUF_Adirelle.RegisterMovable(frame, key, label, mask) end
+		postponed[frame] = { key, label, mask }
 	end
 	
-	oUF_Adirelle.RegisterVariableLoadedCallback(function(db)
+	-- Callback on database loaded/changed
+	oUF_Adirelle.RegisterVariableLoadedCallback(function(profile)
 	
-		local function LM10_Enable(frame) return frame:SetEnabledSetting(true) end
-		local function LM10_Disable(frame) return frame:SetEnabledSetting(false) end
+		-- Get the anchor settings
+		db = profile.anchors
 		
-		-- replace RegisterMovable with a function that actually registers the frame
-		oUF_Adirelle.RegisterMovable = function(frame, key, label, mask)
-			frame:Debug('Registering movable', key, label, mask)
-			db[key] = db[key] or {}
-
-			oUF_Adirelle.RegisterTogglableFrame(frame, key)
-			frame.LM10_IsEnabled = frame.GetEnabledSetting
-			frame.LM10_Enable = LM10_Enable
-			frame.LM10_Disable = LM10_Disable
+		if oUF_Adirelle.RegisterMovable ~= DoRegister then
+			-- First initialization		
+						
+			-- Replace RegisterMovable with the function that actually registers the frame
+			oUF_Adirelle.RegisterMovable = DoRegister
 			
-			libmovable.RegisterMovable(addonName, frame, db[key], label, mask)
-		end
-
-		-- process already registered frames
-		for _, func in pairs(postponed) do func()	end
-		postponed = nil
+			-- Process already registered frames
+			for frame, params in pairs(postponed) do
+				DoRegister(frame, unpack(params))
+			end
+			postponed = nil			
+			
+		else
+			-- Already initialized, only apply the new layout
+			libmovable.UpdateLayout(addonName)
+		end		
 	end)
 
 	-- Register the slash command
--- support/SavedVariables.lua
+++ support/SavedVariables.lua
@@ -9,54 +9,88 @@ local oUF_Adirelle, assert = _G.oUF_Adirelle, _G.assert
 local oUF = assert(oUF_Adirelle.oUF, "oUF is undefined in oUF_Adirelle")
 
 -- Make most globals local so I can check global leaks using "luac -l | grep GLOBAL"
-local ipairs, tinsert = _G.ipairs, _G.tinsert
+local type, pairs, ipairs, tinsert = _G.type, _G.pairs, _G.ipairs, _G.tinsert
+
+local GetLib = oUF_Adirelle.GetLib
 
 local callbacks = {}
 local db
+
+local DEFAULTS = {
+	profile = {
+		anchors = { ['*'] = {} },
+		disabled = { ['*'] = false },
+	}
+}
+
+local function OnDatabaseChanged(event)
+	-- Call all the callbacks
+	for _, callback in ipairs(callbacks) do
+		callback(db.profile)
+	end
+	
+	-- Update all the frames
+	for _, frame in ipairs(oUF.objects) do
+		frame:UpdateAllElements(event)
+	end	
+end
 
 local frame = _G.CreateFrame("Frame")
 frame:SetScript('OnEvent', function(self, event, name)
 	if name ~= addonName then return end
 	self:UnregisterEvent('ADDON_LOADED')
 	self:SetScript('OnEvent', nil)
-
+	
 	-- Initialize the database
-	_G.oUF_Adirelle_DB = _G.oUF_Adirelle_DB or {}
-	db = _G.oUF_Adirelle_DB
-	if not db.disabled then db.disabled = {} end
+	db = GetLib('AceDB-3.0'):New("oUF_Adirelle_DB2", DEFAULTS, true)
+	
+	-- Convert the old database
+	if type(_G.oUF_Adirelle_DB) == "table" then
+		local old = _G.oUF_Adirelle_DB
+		if old.disabled then
+			for k,v in pairs(old.disabled) do
+				db.profile.disabled[k] = not not v
+			end
+		end
+		for key, pos in pairs(old) do
+			if key ~= "disable" and type(pos) == "table" and (pos.pointFrom or pos.pointTo or point.refFrame or point.xOffset or point.yOffset) then
+				for k,v in pairs(pos) do
+					db.profile.anchors[key][k] = v
+				end
+			end
+		end
+		_G.oUF_Adirelle_DB = nil
+	end
+	
+	-- Register AceDB callbacks
+	db.RegisterCallback(self, "OnNewProfile", OnDatabaseChanged)
+	db.RegisterCallback(self, "OnProfileChanged", OnDatabaseChanged)
+	db.RegisterCallback(self, "OnProfileCopied", OnDatabaseChanged)
+	db.RegisterCallback(self, "OnProfileReset", OnDatabaseChanged)
 
 	-- Call the callbacks
-	for _, callback in ipairs(callbacks) do
-		callback(db)
-	end
-	callbacks = nil
-	
-	-- Update all the frames
-	for _, frame in ipairs(oUF.objects) do
-		frame:UpdateAllElements("ADDON_LOADED")
-	end
+	return OnDatabaseChanged('ADDON_LOADED')
 end)
 frame:RegisterEvent('ADDON_LOADED')
 
 -- Register a func to be called once the saved variables are loaded
 function oUF_Adirelle.RegisterVariableLoadedCallback(callback)
+	tinsert(callbacks, callback)
 	if db then
-		callback(db)
-	else
-		tinsert(callbacks, callback)
+		callback(db.profile)
 	end
 end
 
 -- Register a frame so it can be disabled
 local function Frame_GetEnabledSetting(self)
-	return not (db and db.disabled[self.dbKey])
+	return not (db and db.profile.disabled[self.dbKey])
 end
 
 local function Frame_SetEnabledSetting(self, enabled)
 	if db then
 		local disabled = not enabled
-		if db.disabled[self.dbKey] == disabled then return end
-		db.disabled[self.dbKey] = disabled
+		if db.profile.disabled[self.dbKey] == disabled then return end
+		db.profile.disabled[self.dbKey] = disabled
 	end
 	if enabled then
 		self:Enable()
@@ -73,5 +107,5 @@ function oUF_Adirelle.RegisterTogglableFrame(frame, key)
 	frame.Disable = frame.Disable or frame.Hide
 	frame.GetEnabledSetting = Frame_GetEnabledSetting
 	frame.SetEnabledSetting = Frame_SetEnabledSetting
-	oUF_Adirelle.RegisterVariableLoadedCallback(function() if not frame:GetEnabledSetting() then frame:Disable() end end)
+	oUF_Adirelle.RegisterVariableLoadedCallback(function() if not frame:GetEnabledSetting() then frame:Disable() else frame:Enable() end end)
 end
