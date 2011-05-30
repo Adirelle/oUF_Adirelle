15d5fa7f818629df17dbefb0d56c68efedcdf10b
Adirelle <adirelle@gmail.com>
2011-05-30 15:32:10 +0200

    Do not layout auras twice each time the single layout settings are modified.

--- modules/single/Style.lua
+++ modules/single/Style.lua
@@ -388,9 +388,6 @@ local function OnSingleLayoutModified(self, event, layout, theme)
 	end
 	if self:CanChangeProtectedState() and (self:GetWidth() ~= width or self:GetHeight() ~= height) then
 		self:SetSize(width, height)
-		if self.Buffs then
-			return OnAuraLayoutModified(self, event, layout)
-		end
 	end
 end
 
