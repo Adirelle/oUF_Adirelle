85c5c5dd04c8798668f23eee7b99644dc11d42a9
Adirelle <adirelle@gmail.com>
2013-01-01 20:45:42 +0100

    First version of the Powers element.

---
+++ elements/Powers.lua
@@ -0,0 +1,181 @@
+ --[=[
+Adirelle's oUF layout
+(c) 2009-2012 Adirelle (adirelle@gmail.com)
+All rights reserved.
+
+Elements handled: Powers
+
+Options
+
+	.frequentUpdates - Set to true to listen to UNIT_POWER_FREQUENT instead of UNIT_POWER
+
+Sub-widgets
+
+	.MANA .RAGE .FOCUS .ENERGY .RUNIC_POWER .SOUL_SHARDS
+	.HOLY_POWER .CHI .SHADOW_ORBS .BURNING_EMBERS .DEMONIC_FURY
+		There widgets are used to display the various powers.
+
+Power sub-widget "interface"
+
+	The power sub-widgets should implement the following methods:
+		:SetMinMaxValues(min, max)
+		:SetValue(current)
+		:SetStatusBarColor(r, g, b, a)
+
+Sub-widget hooks
+
+	:Override(event, ...)           - Totally override the update.
+	:PreUpdate(unit)                - Called just before updating
+	:PostUpdate(unit, current, max) - Called just after updating
+
+--]=]
+
+local _G, addonName, private = _G, ...
+local oUF_Adirelle, assert = _G.oUF_Adirelle, _G.assert
+local oUF = assert(oUF_Adirelle.oUF, "oUF is undefined in oUF_Adirelle")
+
+--<GLOBALS
+local _G = _G
+--GLOBALS>
+
+local function ShouldShow(unit, powerIndex)
+	return not UnitIsConnected() and UnitPowerType(unit) ~= powerIndex and (UnitPowerMax(unit, powerIndex) or 0) ~= 0
+end
+
+local function Update(self, event)
+	local unit, powerIndex = self.__owner.unit, self.powerIndex
+
+	self:SetShown(ShouldShow(unit, powerIndex))
+	if not self:IsVisible() then
+		return
+	end
+
+	if self.PreUpdate then self:PreUpdate(unit) end
+
+	local current, max = (UnitPower(unit, powerIndex) or 0), (UnitPowerMax(unit, powerIndex) or 0)
+	self:SetMinMaxValues(0, max)
+	self:SetValue(current)
+
+	local t, r, g, b = oUF.colors.power[self.powerType]
+	if not t or self.colorSmooth then
+		r, g, b = self.ColorGradient(current, max, unpack(self.smoothGradient or oUF.colors.smooth))
+	else
+		r, g, b = unpack(t, 1, 3)
+	end
+
+	self:SetStatusBarColor(r, g, b)
+	local bg = self.bg
+	if bg then
+		local mu = bg.multiplier or 1
+		bg:SetVertexColor(r * mu, g * mu, b * mu)
+	end
+
+	if self.PostUpdate then self:PostUpdate(unit, current, max) end
+end
+
+-- Handled powers
+local HandledPowers = {
+	MANA = true,
+	RAGE = true,
+	FOCUS = true,
+	ENERGY = true,
+	--RUNES = true,
+	RUNIC_POWER = true,
+	SOUL_SHARDS = true,
+	--ECLIPSE = true,
+	HOLY_POWER = true,
+	--ALTERNATE_POWER = true,
+	--DARK_FORCE = true,
+	CHI = true,
+	SHADOW_ORBS = true,
+	BURNING_EMBERS = true,
+	DEMONIC_FURY = true,
+}
+for powerType in pairs(HandledPowers) do
+	HandledPowers[powerType] = _G['SPELL_POWER_'..powerType]
+end
+
+local CommonPath = function(self, event, unit, ...)
+	if unit ~= self.unit then return end
+	local powers = self.Powers
+	for powerType in pairs(HandledPowers) do
+		local widget = powers[powerType]
+		if widget then
+			(widget.Override or Update)(widget, event, unit, ...)
+		end
+	end
+end
+
+local CommonForceUpdate = function(element)
+	return CommonPath(element.__owner.Powers, 'ForceUpdate', element.__owner.unit)
+end
+
+local CommonPowerPath = function(self, event, unit, powerType, ...)
+	if unit ~= self.unit then return end
+	local widget = self.powers[powerType]
+	if widget then
+		return (widget.Override or Update)(widget, event, unit, powerType, ...)
+	end
+end
+
+local CommonEnable = function(self, unit)
+	local powers = self.Powers
+	if powers then
+		powers.__owner = self
+		powers.ForceUpdate = CommonForceUpdate
+		local found = false
+		for powerType, powerIndex in pairs(HandledPowers) do
+			local widget = powers[powerType]
+			if widget then
+				widget.__owner = self
+				widget.powerType = powerType
+				widget.powerIndex = powerIndex
+				widget:Show()
+				found = true
+			end
+		end
+
+		if self.SetShown then
+			self:SetShown(found)
+		end
+		if not found then
+			return false
+		end
+
+		if self.frequentUpdates and (unit == 'player' or unit == 'pet') then
+			self:RegisterEvent('UNIT_POWER_FREQUENT', CommonPowerPath)
+		else
+			self:RegisterEvent('UNIT_POWER', CommonPowerPath)
+		end
+		self:RegisterEvent('UNIT_MAXPOWER', CommonPowerPath)
+
+		self:RegisterEvent('UNIT_DISPLAYPOWER', CommonPath)
+		self:RegisterEvent('UNIT_CONNECTION', CommonPath)
+
+		return true
+	end
+end
+
+local CommonDisable = function(self)
+	local powers = self.Powers
+	if powers then
+		for powerType in pairs(HandledPowers) do
+			if powers[powerType] then
+				powers[powerType]:Hide()
+			end
+		end
+
+		self:UnregisterEvent('UNIT_POWER_FREQUENT', CommonPowerPath)
+		self:UnregisterEvent('UNIT_POWER', CommonPowerPath)
+		self:UnregisterEvent('UNIT_MAXPOWER', CommonPowerPath)
+
+		self:UnregisterEvent('UNIT_DISPLAYPOWER', CommonPath)
+		self:UnregisterEvent('UNIT_CONNECTION', CommonPath)
+
+		if self.Hide then
+			self:Hide()
+		end
+	end
+end
+
+oUF:AddElement('Powers', CommonPath, CommonEnable, CommonDisable)
