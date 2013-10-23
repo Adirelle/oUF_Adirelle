6ab8dec50b3b0d9945251157bc1807456813fecd
Adirelle <adirelle@gmail.com>
2013-10-24 00:34:29 +0200

    Do not display debuff created by allies.

--- modules/raid/AuraFilters.lua
+++ modules/raid/AuraFilters.lua
@@ -125,7 +125,7 @@ oUF:AddAuraFilter("CureableDebuff", function(unit)
 	repeat
 		index = index + 1
 		local thisName, _, thisTexture, thisCount, thisDebuffType, thisDuration, thisExpirationTime, caster, _, _, spellID, _, isBossDebuff = UnitDebuff(unit, index)
-		if thisName and thisDuration and thisDuration > 0 then
+		if thisName and thisDuration and thisDuration > 0 and not UnitCanAssist(caster or "", "player") then
 
 			if not thisCount then
 				thisCount = 0
