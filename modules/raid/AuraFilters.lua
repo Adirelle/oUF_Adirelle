2c14db301bbbbf8ea1548405cb9c0751fdf83aaf
Adirelle <adirelle@gmail.com>
2013-11-15 10:59:26 +0100

    Worked again on debuff display, using latest LibDispellable changes.

--- modules/raid/AuraFilters.lua
+++ modules/raid/AuraFilters.lua
@@ -125,11 +125,10 @@ oUF:AddAuraFilter("CureableDebuff", function(unit)
 	repeat
 		index = index + 1
 		local thisName, _, thisTexture, thisCount, thisDebuffType, thisDuration, thisExpirationTime, caster, _, _, spellID, _, isBossDebuff = UnitDebuff(unit, index)
-		if thisName and thisDuration and thisDuration > 0 and LibDispellable:GetDispelType(thisDebuffType, spellID) then
+		if thisName and LibDispellable:IsDispellable(thisDebuffType, spellID) then
 
-			if not thisCount then
-				thisCount = 0
-			end
+			thisCount = thisCount or 0
+			thisDuration = thisDuration or 0
 
 			if isBossDebuff then
 				thisPriority = 50
