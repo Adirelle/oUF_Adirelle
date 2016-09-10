6c0a092430a3f38dea8ff29871aacf6d15d65c98
Adirelle <adirelle@gmail.com>
2016-09-10 14:22:15 +0200

    Relicensed wth GPLv3.

--- elements/Dragon.lua
+++ elements/Dragon.lua
@@ -1,7 +1,19 @@
 --[=[
 Adirelle's oUF layout
 (c) 2009-2016 Adirelle (adirelle@gmail.com)
-All rights reserved.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 Elements handled: .Dragon
 --]=]
@@ -58,4 +70,3 @@ local function Disable(self)
 end
 
 oUF:AddElement('Dragon', Update, Enable, Disable)
-
