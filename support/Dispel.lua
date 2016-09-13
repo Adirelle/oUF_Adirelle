d9b20a979f69c6e18f3860a06493607f6dbf4e6b
Adirelle <adirelle@gmail.com>
2016-09-13 18:18:34 +0200

    Simplified dispel-related code.

--- support/Dispel.lua
+++ support/Dispel.lua
@@ -38,51 +38,56 @@ local C = LPS.constants
 local Dispels = {}
 oUF_Adirelle.Dispels = Dispels
 
-local base = bor(C.DISPEL, C[oUF_Adirelle.playerClass])
-for i, target in pairs{C.HELPFUL, C.HARMFUL, C.PERSONAL, C.PET} do
-    for spellID, _, _, _, _, _, dispels in LPS:IterateSpells(nil, bor(base, target)) do
-        Dispels[spellID] = bor(target, dispels)
-    end
+for spellID, flags, _, _, _, _, dispels in LPS:IterateSpells("HELPFUL HARMFUL PERSONAL PET", "DISPEL "..oUF_Adirelle.playerClass) do
+    Dispels[spellID] = {band(flags, C.TARGETING), dispels}
 end
 
-local function noop() return end
+local function noop() end
 
 if next(Dispels) == nil then
-    -- This class cannot dispel, use short funcs
-    oUF_Adirelle.IsDispellable =  function() return false end
-    oUF_Adirelle.CanDispel = oUF_Adirelle.IsDispellable
+    -- Player cannot dispel: define dummy functions and bail out
+    oUF_Adirelle.IsDispellable = noop
+    oUF_Adirelle.CanDispel = noop
     oUF_Adirelle.IterateDispellableAuras = function() return noop end
     return
 end
 
 local DispelFlags = { Magic = C.MAGIC, Poison = C.POISON, Curse = C.CURSE, Disease = C.DISEASE }
-local DispelByType = { Magic = 0, Poison = 0, Curse = 0, Disease = 0 }
+local TargetsByType = { Magic = 0, Poison = 0, Curse = 0, Disease = 0 }
 
 -- Update the tests according to known spells
 oUF_Adirelle:RegisterEvent('SPELLS_CHANGED', function()
-    for k in pairs(DispelByType) do
-        DispelByType[k] = 0
+    for k in pairs(TargetsByType) do
+        TargetsByType[k] = 0
     end
-    for spellID, flags in pairs(Dispels) do
+    for spellID, data in pairs(Dispels) do
         if IsSpellKnown(spellID, false) or IsSpellKnown(spellID, true) then
+            local targets, dispels = unpack(data)
             for t, f in pairs(DispelFlags) do
-                DispelByType[t] = bor(DispelByType[t], flags)
+                if band(dispels, f) ~= 0 then
+                    TargetsByType[t] = bor(TargetsByType[t], targets)
+                end
             end
         end
 	end
 end)
 
-local function UnitCat(unit)
+local PLAYER = bor(C.PERSONAL, C.HELPFUL)
+local PET = bor(C.PET, C.HELPFUL)
+local FRIEND = C.HELPFUL
+local FOE = C.HARMFUL
+
+local function UnitTargetType(unit)
     if type(unit) ~= "string" or unit == "" then
         return 0
     elseif UnitIsUnit(unit, "player") then
-        return bor(C.PERSONAL, C.HELPFUL), false
+        return PLAYER, false
     elseif UnitIsUnit(unit, "pet") then
-        return bor(C.PET, C.HELPFUL), false
+        return PET, false
     elseif UnitCanAttack(unit, "player") then
-        return C.HARMFUL, true
+        return FOE, true
     elseif UnitCanAssist("player", unit) then
-        return C.HELPFUL, false
+        return FRIEND, false
     end
     return 0
 end
@@ -92,17 +97,15 @@ function oUF_Adirelle.IsDispellable(debuffType)
 end
 
 function oUF_Adirelle.CanDispel(unit, isBuff, debuffType)
-    local flags = debuffType and DispelByType[debuffType]
-    if not flags then return false end
-    local unitCat, forBuff = UnitCat(unit)
-    return isBuff == forBuff and band(flags, unitCat) ~= 0
+    local targets = debuffType and TargetsByType[debuffType]
+    if not targets then return false end
+    local targetType, auraType = UnitTargetType(unit)
+    return auraType == isBuff and band(targets, targetType) ~= 0
 end
 
 function oUF_Adirelle.IterateDispellableAuras(unit, buffs)
-    local unitCat = UnitCat(unit)
-    local offensive = (unitCat == C.HARMFUL)
-    if offensive ~= buffs then
-        -- Cannot dispel enemy debuffs nor ally buffs
+    local targetType, auraType = UnitTargetType(unit)
+    if auraType ~= buffs then
         return noop
     end
     local getAura = buffs and UnitBuff or UnitDebuff
@@ -111,7 +114,7 @@ function oUF_Adirelle.IterateDispellableAuras(unit, buffs)
             index = index + 1
             local name, rank, icon, count, debuffType, duration, expires, caster, isStealable, shouldConsolidate, spellID, canApplyAura, isBossDebuff = getAura(index)
             if name and debuffType and DispelFlags[debuffType] then
-                local canDispel = band(DispelByType[debuffType], unitCat) ~= 0
+                local canDispel = band(TargetsByType[debuffType], targetType) ~= 0
                 return index, name, canDispel, rank, icon, count, debuffType, duration, expires, caster, isStealable, shouldConsolidate, spellID, canApplyAura, isBossDebuff
             end
         until not name
