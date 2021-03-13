62cba800ace5de2052a85702410e119a8edc5bca
Adirelle <adirelle@gmail.com>
2021-03-13 21:39:53 +0100

    Disable personal resource widgets on player nameplate.

--- src/modules/nameplates/Setup.lua
+++ src/modules/nameplates/Setup.lua
@@ -20,6 +20,10 @@ local _G = _G
 local oUF_Adirelle, assert = _G.oUF_Adirelle, _G.assert
 local oUF = assert(oUF_Adirelle.oUF, "oUF is undefined in oUF_Adirelle")
 
+local NamePlateDriverFrame = _G.NamePlateDriverFrame
+local hooksecurefunc = _G.hooksecurefunc
+local SetCVar = _G.SetCVar
+
 local NAMEPLATE_CVARS = oUF_Adirelle.NAMEPLATE_CVARS
 
 local function LoadNameplateCVars()
@@ -32,8 +36,36 @@ local function LoadNameplateCVars()
 	end
 end
 
+local function insecureOnShow(frame)
+	frame:Hide()
+end
+
+local function DisableFrame(frame)
+	if not frame then
+		return
+	end
+	frame:HookScript("OnShow", insecureOnShow)
+	frame:UnregisterAllEvents()
+	frame:Hide()
+end
+
+local function DisableClassNameplateBar()
+	DisableFrame(NamePlateDriverFrame:GetClassNameplateBar())
+end
+
+local function DisableClassNameplateManaBar()
+	DisableFrame(NamePlateDriverFrame:GetClassNameplateManaBar())
+end
+
 oUF:Factory(function()
 	oUF:SetActiveStyle("Adirelle_Nameplate")
 	oUF:SpawnNamePlates("oUF_Adirelle_")
+
 	LoadNameplateCVars()
+
+	hooksecurefunc(NamePlateDriverFrame, "SetClassNameplateBar", DisableClassNameplateBar)
+	DisableClassNameplateBar()
+
+	hooksecurefunc(NamePlateDriverFrame, "SetClassNameplateManaBar", DisableClassNameplateManaBar)
+	DisableClassNameplateManaBar()
 end)
